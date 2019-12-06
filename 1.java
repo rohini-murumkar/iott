e0af272504dc03ef2cd47519c35285d00d9c396f
rohini <rohinimurumkar00@gmail.com>
2019-12-06 01:31:54 -0800

    changr

---
+++ 1.java
@@ -0,0 +1,29 @@
+
+Java Program to Print Star Pyramid Patterns
+« Previous Program Next Program »
+
+Print Patterns of Numbers and Stars
+
+To print patterns of numbers and stars (*) in Java Programming, you have to use two loops, first is outer loop and the second is inner loop. The outer loop is responsible for rows and the inner loop is responsible for columns.
+Java Programming Code to Prints Patterns
+
+Following are the one by one Java Programs to print different-different patterns :
+Java Program - Pattern 1
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int i, j;
+        for(i=0; i<5; i++)
+        {
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println(hi);
+        }
+    }
+}
+
+
